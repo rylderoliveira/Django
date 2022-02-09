a2c0441974ca214aee5df3f063117f2751188c5d
Rylder Oliveira <rylder2014@gmail.com>
2022-02-09 16:02:29 -0300

    terminando verificação de login

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
+from loja.views import index
 
 urlpatterns = [
     path('cadastroUsuario', views.cadastroUsuario, name='cadastroUsuario'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', index, name='dashboard')
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,19 +1,60 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def cadastroUsuario(request):
     if request.method=="POST":
         usuario = request.POST['user']
         senha = request.POST['password']
+
+        # Validações
+        if not usuario.strip():
+            print('Usuario Invalido')
+            return redirect('cadastroUsuario')
+        
+        if not senha.strip():
+            print('Senha Invalida')
+            return redirect('cadastroUsuario')
+
+        if User.objects.filter(username=usuario).exists():
+            print('Usuario já cadastrado')
+            return redirect('cadastroUsuario')
+        
+        user = User.objects.create_user(username=usuario, password=senha)
+        user.save()
+        print('Usuario cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuario/cadastroUsuario.html')
 
 def login(request):
-    return render(request, 'usuario/telaLogin.html')
+    if request.method=="POST":
+        usuario = request.POST['user']
+        senha = request.POST['password']
+
+        #Validações
+        if usuario == '' or senha == '':
+            print('Usuario ou senha invalidos')
+            return redirect('login')
+
+        if User.objects.filter(username=usuario).exists():
+            user = auth.authenticate(request, username=usuario, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
+            else:
+                return redirect('login')
+    else:
+        return render(request, 'usuario/telaLogin.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 def dashboard(request):
-    pass
+    if request.user.is_authenticated():
+        return render(request, 'usuario/dashboard.html')
+    else:
+        return redirect('index')
