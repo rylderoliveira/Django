23c966a96f5877a9402d13bc237f3ddb00769d38
Rylder Oliveira <rylder2014@gmail.com>
2022-02-14 12:59:44 -0300

    Adicionando mensagem de erro e de sucesso

--- loja/views.py
+++ loja/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from loja.forms import ProductForm
 from .models import Produto
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -28,7 +29,12 @@ def editarProduto(request, produto_id):
         form = ProductForm(request.POST, instance=produto)
         if form.is_valid():
             form.save()
+            messages.success(request, 'O produto %s foi editado com sucesso' %request.POST['nome'])
             return redirect('lista')
+        else:
+            messages.error(request, 'Houve um erro na validação dos dados')
+            return render(request, 'editarProduto.html', context)
+
 
     context = {
         'form': form
--- usuario/views.py
+++ usuario/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 from loja.models import Produto
 from loja.forms import ProductForm
+from django.contrib import messages
 
 # Create your views here.
 def cadastroUsuario(request):
@@ -12,20 +13,21 @@ def cadastroUsuario(request):
 
         # Validações
         if not usuario.strip():
+            messages.error(request, 'Usuario inválido')
             print('Usuario Invalido')
             return redirect('cadastroUsuario')
         
         if not senha.strip():
-            print('Senha Invalida')
+            messages.error(request, 'Senha inválida')
             return redirect('cadastroUsuario')
 
         if User.objects.filter(username=usuario).exists():
-            print('Usuario já cadastrado')
+            messages.error(request, 'Usuario já cadastrado')
             return redirect('cadastroUsuario')
         
         user = User.objects.create_user(username=usuario, password=senha)
         user.save()
-        print('Usuario cadastrado com sucesso')
+        messages.success(request, 'Cadastro realizado com sucesso')
         return redirect('login')
     else:
         return render(request, 'usuario/cadastroUsuario.html')
@@ -36,15 +38,15 @@ def login(request):
         senha = request.POST['password']
 
         #Validações
-        if usuario == '' or senha == '':
-            print('Usuario ou senha invalidos')
+        if not usuario.strip() or not senha.strip():
+            messages.error(request, 'Usuario ou senha inválidos')
             return redirect('login')
 
         if User.objects.filter(username=usuario).exists():
             user = auth.authenticate(request, username=usuario, password=senha)
             if user is not None:
                 auth.login(request, user)
-                print('Login realizado com sucesso')
+                messages.success(request, 'Login realizado com sucesso')
                 return redirect('dashboard')
             else:
                 return redirect('login')
@@ -75,7 +77,11 @@ def cadastroProduto(request):
                 publicado = False
             produto = Produto.objects.create(nome=request.POST['nome'], fornecedor=request.POST['fornecedor'], preco=request.POST['preco'], cadastrado_por=user, publicado=publicado)
             produto.save()
+            messages.success(request, 'O produto %s foi cadastrado com sucesso' %request.POST['nome'])
             return redirect('lista')
+        else:
+            messages.error(request, 'Houve um erro na válidação dos dados')
+            return render(request, 'cadastroProduto.html', {'form': form})
     else:
         form = ProductForm
         return render(request, 'cadastroProduto.html', {'form': form})
