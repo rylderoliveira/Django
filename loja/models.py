7fc4c714dcdc049a2a08d4acff992700e5114bf0
Rylder Oliveira <rylder2014@gmail.com>
2022-02-21 15:17:47 -0300

    Criando produto na pagina index

--- loja/models.py
+++ loja/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Produto(models.Model):
     cadastrado_por = models.ForeignKey(User, on_delete=models.CASCADE)
-    nome = models.CharField(max_length=255)
-    fornecedor = models.CharField(max_length=255)
+    nome = models.CharField(max_length=200)
+    fornecedor = models.CharField(max_length=200)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
     publicado = models.BooleanField(default=False)
+    imagem = models.ImageField(default='ProdutoPadrão', blank=True, upload_to='fotos/%d/%m/%Y/', )
